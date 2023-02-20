fafd26af229abae86a3aa89a1e13932ec30e6105
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-02-20 12:06:26 -0600

    move OI code

--- src/main/java/io/github/tigerbotics7125/robot/Robot.java
+++ src/main/java/io/github/tigerbotics7125/robot/Robot.java
@@ -6,7 +6,8 @@
 package io.github.tigerbotics7125.robot;
 
 import com.revrobotics.REVPhysicsSim;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import io.github.tigerbotics7125.tigerlib.CommandRobot;
 
@@ -14,19 +15,37 @@ public class Robot extends CommandRobot {
 
     public static final RobotContainer mRobotContainer = new RobotContainer();
 
+    private double mMatchStartTime = 0.0;
+    private String mMatchTime = "N/A";
+
     @Override
-    public void robotInit() {}
+    public void robotInit() {
+        OperatorInterface.init();
+    }
 
     @Override
     public void robotPeriodic() {
         mRobotContainer.periodic();
+        OperatorInterface.update();
         Shuffleboard.update();
         SmartDashboard.updateValues();
     }
 
     @Override
     public void autonomousInit() {
+        OperatorInterface.startMatchSegment();
         // mRobotContainer.getAutonomousCommand().schedule();
+    }
+
+    @Override
+    public void teleopInit() {
+        OperatorInterface.startMatchSegment();
+    }
+
+    @Override
+    public void simulationInit() {
+        // Disable joystick warnings in sim.
+        DriverStation.silenceJoystickConnectionWarning(true);
     }
 
     @Override
