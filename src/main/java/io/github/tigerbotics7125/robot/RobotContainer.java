e9c7df7051f5e9fcf65e375cdbde7d4e59015c75
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-03-04 17:47:37 -0600

    update many bindings

--- src/main/java/io/github/tigerbotics7125/robot/RobotContainer.java
+++ src/main/java/io/github/tigerbotics7125/robot/RobotContainer.java
@@ -6,30 +6,32 @@
 package io.github.tigerbotics7125.robot;
 
 import static io.github.tigerbotics7125.robot.constants.OIConstants.*;
-import static io.github.tigerbotics7125.robot.constants.RobotConstants.*;
+import static io.github.tigerbotics7125.robot.constants.RobotConstants.PNEUMATICS_MODULE_TYPE;
+import static io.github.tigerbotics7125.tigerlib.input.trigger.Trigger.ActivationCondition.*;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
+import com.pathplanner.lib.auto.MecanumAutoBuilder;
+import com.pathplanner.lib.auto.PIDConstants;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import io.github.tigerbotics7125.robot.commands.AutoPilot;
-import io.github.tigerbotics7125.robot.constants.field.FieldArea;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import io.github.tigerbotics7125.lib.AllianceFlipUtil;
+import io.github.tigerbotics7125.robot.auto.Auto;
+import io.github.tigerbotics7125.robot.auto.CenterDelayMobility;
+import io.github.tigerbotics7125.robot.auto.None;
+import io.github.tigerbotics7125.robot.constants.AutoPilotConstants;
 import io.github.tigerbotics7125.robot.subsystem.Drivetrain;
 import io.github.tigerbotics7125.robot.subsystem.Drivetrain.TurningMode;
 import io.github.tigerbotics7125.robot.subsystem.Intake;
+import io.github.tigerbotics7125.robot.subsystem.SuperStructure;
 import io.github.tigerbotics7125.robot.subsystem.Vision;
-import io.github.tigerbotics7125.robot.sysid.logging.SysIdDrivetrainLogger;
-import io.github.tigerbotics7125.robot.sysid.logging.SysIdGeneralMechanismLogger;
 import io.github.tigerbotics7125.tigerlib.input.controller.XboxController;
 import io.github.tigerbotics7125.tigerlib.input.trigger.Trigger;
 import io.github.tigerbotics7125.tigerlib.input.trigger.Trigger.ActivationCondition;
-import java.util.ArrayList;
-import java.util.List;
+import io.github.tigerbotics7125.tigerlib.util.JoystickUtil;
 
 public class RobotContainer {
 
@@ -40,16 +42,10 @@ public class RobotContainer {
     Vision mVision = new Vision();
     Drivetrain mDrivetrain = new Drivetrain();
     Intake mIntake = new Intake();
-    // SuperStructure mSuperStruc = new SuperStructure();
+    SuperStructure mSuperStruc = new SuperStructure();
     Compressor mCompressor = new Compressor(PNEUMATICS_MODULE_TYPE);
 
-    boolean sysIdActive = false;
-    SysIdGeneralMechanismLogger sysIdMechLogger;
-    SysIdDrivetrainLogger sysIdDriveLogger;
-    SendableChooser<SubsystemBase> sysIdMechChooser;
-
-    AutoPilot mAutoPilot = new AutoPilot(mDrivetrain);
-    Field2d mAutoPilotField = new Field2d();
+    AutoPilot mAutoPilot = new AutoPilot(mDrivetrain, OperatorInterface::getAutoPilotPoint);
 
     Field2d mField = new Field2d();
 
@@ -58,12 +54,12 @@ public class RobotContainer {
     public RobotContainer() {
         mCompressor.enableDigital();
         configTriggers();
-        configDashboard();
         configDefaultCommands();
+        configDashboard();
 
-        sysIdMechChooser = new SendableChooser<>();
-        sysIdMechChooser.setDefaultOption(mDrivetrain.getName(), mDrivetrain);
-        Shuffleboard.getTab("sysId").add(sysIdMechChooser);
+        mDrivetrain.setFieldOriented(false);
+        mDrivetrain.setTurningMode(TurningMode.JOYSTICK_DIRECT);
+    }
 
     public Command getAutoCommand() {
 
@@ -90,7 +86,9 @@ public class RobotContainer {
         mDriver.rb().trigger(() -> mDrivetrain.setTurningMode(TurningMode.JOYSTICK_ANGLE));
         mDriver.y().trigger(() -> mDrivetrain.setTurningMode(TurningMode.HEADING_LOCK));
 
-        mDriver.start().activate(ActivationCondition.WHILE_HIGH).trigger(mAutoPilot);
+        mDriver.start()
+                .activate(ActivationCondition.WHILE_HIGH)
+                .trigger(mAutoPilot.getAutoPilotCommand());
 
         // Operator
         mOperator.pov.left().trigger(OperatorInterface.selectLeft());
@@ -103,18 +101,43 @@ public class RobotContainer {
 
         mOperator.start().trigger(OperatorInterface.toggleZone());
 
-        // Subsystems
+        mOperator.a().trigger(mIntake.releaseObject());
+        mOperator.b().trigger(mIntake.grabObject());
+        mOperator.y().trigger(mIntake::intake);
+        mOperator.x().trigger(mIntake::disable);
 
+        mOperator
+                .rb()
+                .activate(WHILE_HIGH)
+                .trigger(
+                        Commands.runOnce(
+                                () ->
+                                        mSuperStruc.armDuty(
+                                                JoystickUtil.clamp(
+                                                        mOperator.rightY().get(), -.2, .2))));
+        mOperator
+                .lb()
+                .activate(WHILE_HIGH)
+                .trigger(
+                        Commands.runOnce(
+                                () ->
+                                        mSuperStruc.wristDuty(
+                                                JoystickUtil.clamp(
+                                                        mOperator.rightY().get(), -.2, .2))));
+        /*
+        mOperator.pov.up().activate(WHILE_HIGH).trigger(Commands.runOnce(() -> mSuperStruc.elevatorDuty(.5)));
+        mOperator.pov.down().activate(WHILE_HIGH).trigger(Commands.runOnce(() -> mSuperStruc.elevatorDuty(-0.5)));
+        */
+
+        mOperator.pov.up().activate(WHILE_HIGH).trigger(mSuperStruc.elevatorUp());
+        mOperator.pov.up().activate(WHILE_HIGH).trigger(mSuperStruc.elevatorDown());
     }
 
     public void configDashboard() {
         Shuffleboard.getTab("odometry").add(mField);
 
-        ShuffleboardTab autoPilotTab = Shuffleboard.getTab("AutoPilot");
-
-        autoPilotTab.addBoolean(
-                "WithinCommunity", () -> FieldArea.COMMUNITY.contains(mDrivetrain.getPose()));
-        autoPilotTab.add(mAutoPilotField);
+        mAutoChooser.setDefaultOption("NONE", new None());
+        mAutoChooser.addOption("Center Delay to Wall", new CenterDelayMobility());
     }
 
     private void configDefaultCommands() {
@@ -124,28 +147,23 @@ public class RobotContainer {
                         mDriver.leftX()::get,
                         mDriver.rightY()::get,
                         mDriver.rightX()::get,
-                        false));
+                        true));
+
+        mIntake.setDefaultCommand(Commands.run(mIntake::disable, mIntake));
+        mSuperStruc.setDefaultCommand(Commands.run(mSuperStruc::disable, mSuperStruc));
     }
 
     /** Periodic call, always runs. */
     public void periodic() {
-        mAutoPilot.generatePath();
 
-        mVision.getEstimatedPose().ifPresent(mDrivetrain::addVisionEstimate);
+        // mVision.getEstimatedPose().ifPresent(mDrivetrain::addVisionEstimate);
 
         mField.setRobotPose(mDrivetrain.getPose());
-        mAutoPilotField.setRobotPose(mDrivetrain.getPose());
-
-        List<Pose2d> trajPoses = new ArrayList<>();
-        // DS always on trajectory
-        mAutoPilot.getTrajectory().getStates().forEach((state) -> trajPoses.add(state.poseMeters));
-        mAutoPilotField.getObject("GenPath").setPoses(trajPoses);
-
-        mAutoPilotField
-                .getObject("CHARGING_STATION")
-                .setPoses(FieldArea.CHARGING_STATION.getPoses());
-        mAutoPilotField.getObject("LOADING_ZONE").setPoses(FieldArea.LOADING_ZONE.getPoses());
-        mAutoPilotField.getObject("COMMUNITY").setPoses(FieldArea.COMMUNITY.getPoses());
+        mField.getObject("Flipped").setPose(AllianceFlipUtil.apply(mDrivetrain.getPose()));
+        mField.getObject("Community")
+                .setPoses(AutoPilotConstants.AutoPilotBoundary.COMMUNITY.getPoses());
+        mField.getObject("LoadingZone")
+                .setPoses(AutoPilotConstants.AutoPilotBoundary.LOADING_ZONE.getPoses());
     }
 
     /** Periodic call, only runs during simulation. */
