215431b4f2f5ac48d73272bbd64a27d31a4e7c87
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-03-11 09:52:41 -0600

    continue "commandifying" intake

--- src/main/java/io/github/tigerbotics7125/robot/constants/IntakeConstants.java
+++ src/main/java/io/github/tigerbotics7125/robot/constants/IntakeConstants.java
@@ -20,12 +20,6 @@ public class IntakeConstants {
     public static final int SLAVE_ID = 42;
     public static final int FORWARDS_CHANNEL = 0;
     public static final int REVERSE_CHANNEL = 1;
-    public static final int OCCUPANCY_1_PORT = 1;
-    public static final int OCCUPANCY_2_PORT = 2;
-
-    // Occupancy settings
-    public static final double OCCUPANCY_VOLTAGE_THRESHOLD = 2.5;
-    public static final double OCCUPANCY_DEBOUNCE_TIME = .2; // seconds
 
     // Characteristics
     public static final float STALL_TORQUE_NEWTON_METERS = 19.40175484f;
@@ -34,7 +28,15 @@ public class IntakeConstants {
     // Motor values
     public static final MotorType MOTOR_TYPE = MotorType.kBrushless;
     public static final IdleMode IDLE_MODE = IdleMode.kCoast;
-    public static final int STALL_CURRENT_LIMIT_AMPS = 8;
-    public static final int FREE_SPEED_CURRENT_LIMIT_AMPS = 2;
-    public static final double OPEN_LOOP_RAMP_RATE = 0.05;
+    public static final int STALL_CURRENT_LIMIT_AMPS = 20;
+    public static final int FREE_SPEED_CURRENT_LIMIT_AMPS = 4;
+
+    public static final double INTAKE_IN_SPEED = 1D;
+    public static final double INTAKE_OUT_SPEED = -1D;
+    public static final double INTAKE_HOLD_SPEED = .05;
+    public static final double INTAKE_STALL_CURRENT = 15;
+
+    // Pneumatic values
+    public static final DoubleSolenoid.Value GRIP_DIRECTION = DoubleSolenoid.Value.kForward;
+    public static final DoubleSolenoid.Value RELEASE_DIRECTION = DoubleSolenoid.Value.kReverse;
 }
--- src/main/java/io/github/tigerbotics7125/robot/subsystem/Intake.java
+++ src/main/java/io/github/tigerbotics7125/robot/subsystem/Intake.java
@@ -7,18 +7,24 @@ package io.github.tigerbotics7125.robot.subsystem;
 
 import static io.github.tigerbotics7125.robot.constants.IntakeConstants.*;
 import static io.github.tigerbotics7125.robot.constants.RobotConstants.PNEUMATICS_MODULE_TYPE;
+import static io.github.tigerbotics7125.tigerlib.input.trigger.Trigger.ActivationCondition.*;
+import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.REVPhysicsSim;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import io.github.tigerbotics7125.robot.PowerMonitor;
 import io.github.tigerbotics7125.robot.Robot;
 import io.github.tigerbotics7125.robot.constants.RobotConstants;
 import io.github.tigerbotics7125.tigerlib.input.trigger.Trigger;
@@ -27,7 +33,12 @@ public class Intake extends SubsystemBase {
     CANSparkMax mMaster;
     CANSparkMax mSlave;
 
+    SparkMaxPIDController mController;
+
     DoubleSolenoid mSolenoid;
+
+    LinearFilter mCurrentFilter = LinearFilter.movingAverage(10);
+    double mOutputCurrent = 0.0;
 
     ShuffleboardTab mSBTab;
 
@@ -36,16 +47,14 @@ public class Intake extends SubsystemBase {
         mSlave = new CANSparkMax(SLAVE_ID, MotorType.kBrushless);
         mSlave.follow(mMaster, true);
 
-        mSolenoid = new DoubleSolenoid(PNEUMATICS_MODULE_TYPE, FORWARDS_CHANNEL, REVERSE_CHANNEL);
+        mController = mMaster.getPIDController();
 
-        double ampThreshold = 2;
-        new Trigger(() -> PowerMonitor.getInstance().getCurrent(5) > ampThreshold)
-                .trigger(this.grabObject());
+        mSolenoid = new DoubleSolenoid(PNEUMATICS_MODULE_TYPE, FORWARDS_CHANNEL, REVERSE_CHANNEL);
 
         mSBTab = Shuffleboard.getTab("Intake");
         mSBTab.addBoolean("Claw", () -> mSolenoid.get().equals(Value.kForward));
-        mSBTab.addDouble("Master Motor Current", () -> PowerMonitor.getInstance().getCurrent(5));
-        mSBTab.addDouble("Slave Current", () -> PowerMonitor.getInstance().getCurrent(6));
+        mSBTab.addDouble("Master Motor Current", () -> Robot.mPDH.getCurrent(5));
+        mSBTab.addDouble("Slave Current", () -> Robot.mPDH.getCurrent(6));
     }
 
     /**
@@ -60,8 +69,6 @@ public class Intake extends SubsystemBase {
         motor.setSmartCurrentLimit(STALL_CURRENT_LIMIT_AMPS, FREE_SPEED_CURRENT_LIMIT_AMPS, 0);
         motor.enableVoltageCompensation(RobotConstants.NOMINAL_VOLTAGE);
 
-        motor.setOpenLoopRampRate(OPEN_LOOP_RAMP_RATE);
-
         motor.burnFlash();
 
         if (Robot.isSimulation())
@@ -71,49 +78,89 @@ public class Intake extends SubsystemBase {
 
     /** Disables all motor and pneumatic output. */
     public void disable() {
-        this.open();
         mMaster.disable();
         mSlave.disable();
     }
 
-    /**
-     * @return A Command which will close the intake, and run the wheels slowly as to keep the
-     *     object from slipping out.
-     */
-    public Command grabObject() {
-        var command =
-                Commands.sequence(
-                        Commands.run(this::intake).withTimeout(.15),
-                        Commands.parallel(Commands.runOnce(this::close), Commands.run(this::grip)));
-        command.addRequirements(this);
-        return command;
+    public double getCurrent() {
+        return mOutputCurrent;
     }
 
     /**
-     * @return A Command which will open the intake, and run the wheels backwords as to get rid of a
-     *     held object.
+     *
+     * @param grip Whether to grip or not grip the grippers.
+     * @return A Command which will control the gripper pneumatics.
      */
-    public Command releaseObject() {
-        var command =
-                Commands.sequence(
-                        Commands.run(this::eject).withTimeout(.15), Commands.runOnce(this::open));
-        command.addRequirements(this);
-        return command;
+    public CommandBase setGrippers(boolean grip) {
+        return runOnce(() -> mSolenoid.set(grip ? GRIP_DIRECTION : RELEASE_DIRECTION));
     }
 
-    /** Close the intake. */
-    public void close() {
-        mSolenoid.set(Value.kForward);
+    /**
+     *
+     * @param dutyCycle The duty cycle to run the motor at, [-1, 1].
+     * @return A Command which will run the intake motors.
+     */
+    public CommandBase runIntake(double dutyCycle) {
+        return run(() -> mController.setReference(dutyCycle, ControlType.kDutyCycle));
     }
 
-    /** Open the intake. */
-    public void open() {
-        mSolenoid.set(Value.kReverse);
+    /**
+     *
+     * @return A Command which will open the gripper.
+     */
+    public CommandBase grippersOpen() {
+        return setGrippers(false);
     }
 
-    /** Set the motors to intake. */
-    public void intake() {
-        mMaster.set(1);
+    /**
+     *
+     * @return A Command which will lose the gripper.
+     */
+    public CommandBase grippersClose() {
+        return setGrippers(true);
+    }
+
+    /**
+     *
+     * @return A Command which will run the intake inwards for collection.
+     */
+    public CommandBase intakeIn() {
+        return runIntake(INTAKE_IN_SPEED);
+    }
+
+    /**
+     *
+     * @return A Command which will run the intake outwards for ejection.
+     */
+    public CommandBase intakeOut() {
+        return runIntake(INTAKE_OUT_SPEED);
+    }
+
+    /**
+     *
+     * @return A Command which will run the intake inwards very slowly, to hold a gamepiece.
+     */
+    public CommandBase intakeHold() {
+        return runIntake(INTAKE_HOLD_SPEED);
+    }
+
+    /**
+     *
+     * @return A Command which will close the grippers, then run the intake to hold a gamepiece.
+     */
+    public CommandBase holdGamepiece() {
+        return grippersClose().andThen(runIntake(INTAKE_HOLD_SPEED));
+    }
+
+    /**
+     *
+     * @return A Command which will run the intake, then will detect when a gamepiece is intaked, then close and hold it.
+     */
+    public CommandBase intakeAuto() {
+        Debouncer debounce = new Debouncer(1, Debouncer.DebounceType.kRising);
+        return runOnce(() -> debounce.calculate(false)).andThen(grippersOpen()).andThen(intakeIn())
+                .until(() -> debounce.calculate(getCurrent() >= INTAKE_STALL_CURRENT))
+                .finallyDo((interrupted) -> holdGamepiece().schedule());
     }
 
     /** Set the motors to pull inwards slowly. */
@@ -124,5 +171,10 @@ public class Intake extends SubsystemBase {
     /** Set the motors to eject */
     public void eject() {
         mMaster.set(-1);
+    }
+
+    @Override
+    public void periodic() {
+        mOutputCurrent = mCurrentFilter.calculate(mMaster.getOutputCurrent());
     }
 }
