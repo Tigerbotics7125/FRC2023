6a3001ebc23bff8dc72f403706e0a9327608fd7d
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-03-30 23:56:47 -0500

    fix DT base size

--- src/main/java/io/github/tigerbotics7125/robot/constants/RobotConstants.java
+++ src/main/java/io/github/tigerbotics7125/robot/constants/RobotConstants.java
@@ -5,14 +5,17 @@
  */
 package io.github.tigerbotics7125.robot.constants;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class RobotConstants {
 
     public static final double NOMINAL_VOLTAGE = 12.0;
 
-    public static final double ROBOT_WIDTH_METERS = 0.660400;
-    public static final double ROBOT_LENGTH_METERS = 0.820674;
+    public static final double ROBOT_WIDTH_METERS = Units.inchesToMeters(32);
+    public static final double ROBOT_LENGTH_METERS = Units.inchesToMeters(38);
 
     public static final PneumaticsModuleType PNEUMATICS_MODULE_TYPE = PneumaticsModuleType.REVPH;
+
+    public static final double NEO_FREE_SPEED_RPM = 5676;
 }
