e2c8e4705d139045b752360a05b16da464d3d142
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-03-30 23:58:10 -0500

    Create util for advantage scope

---
+++ src/main/java/io/github/tigerbotics7125/lib/ASUtil.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Tigerbotics and it's members. All rights reserved.
+ * This work is licensed under the terms of the GNU GPLv3 license
+ * found in the root directory of this project.
+ */
+package io.github.tigerbotics7125.lib;
+
+import edu.wpi.first.math.geometry.Pose3d;
+
+/** Advantage Scope Utilities. */
+public class ASUtil {
+
+    public static double[] pose3dToDoubleArray(Pose3d pose) {
+        return new double[] {
+            pose.getX(),
+            pose.getY(),
+            pose.getZ(),
+            pose.getRotation().getQuaternion().getW(),
+            pose.getRotation().getQuaternion().getX(),
+            pose.getRotation().getQuaternion().getY(),
+            pose.getRotation().getQuaternion().getZ()
+        };
+    }
+}
