c92a99ce18ff9d5fbae26ff0b3888d0ac2affe84
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-03-30 23:59:37 -0500

    create wrist subsystem

---
+++ src/main/java/io/github/tigerbotics7125/robot/constants/WristConstants.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023 Tigerbotics and it's members. All rights reserved.
+ * This work is licensed under the terms of the GNU GPLv3 license
+ * found in the root directory of this project.
+ */
+package io.github.tigerbotics7125.robot.constants;
+
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
+
+public class WristConstants {
+
+    public static final MotorType MOTOR_TYPE = MotorType.kBrushless;
+
+    public static final int MOTOR_ID = 31;
+    public static final int CANCODER_ID = 31;
+
+    // Theoretical values, neo free speed (in rps) * pos_conv_factor
+    public static final double MAX_VEL = 1.76; // rotations per second
+    // not calculated, simply pick time to reach max vel.
+    public static final double MAX_ACCEL = MAX_VEL / 1D; // rotations per second^2
+
+    public static final double P_GAIN = 1;
+    public static final double I_GAIN = 0;
+    public static final double D_GAIN = 0;
+    public static final Constraints CONSTRAINTS = new Constraints(MAX_VEL, MAX_ACCEL);
+    public static final ProfiledPIDController PID =
+            new ProfiledPIDController(P_GAIN, I_GAIN, D_GAIN, CONSTRAINTS);
+
+    static {
+        PID.setTolerance(Units.degreesToRadians(1));
+    }
+
+    public static final double ABSOLUTE_HOME_DEG = 0D;
+
+    public static final double GEAR_RATIO = 20D;
+    public static final double CHAIN_RATIO = 60D / 12D;
+    public static final double CHAIN_RATIO2 = 12D / 22D;
+    public static final double CANCODER_POS_CONV_FACTOR = 1D / CHAIN_RATIO2;
+    public static final double POS_CONV_FACTOR = 1D / (GEAR_RATIO * CHAIN_RATIO * CHAIN_RATIO2);
+    public static final double VEL_CONV_FACTOR = POS_CONV_FACTOR / 60D; // mps
+
+    public static final CANCoderConfiguration CANCODER_CONFIG = new CANCoderConfiguration();
+
+    static {
+        CANCODER_CONFIG.absoluteSensorRange = AbsoluteSensorRange.Signed_PlusMinus180;
+        CANCODER_CONFIG.initializationStrategy =
+                SensorInitializationStrategy.BootToAbsolutePosition;
+        CANCODER_CONFIG.magnetOffsetDegrees = 0D; // TODO: find this value
+        CANCODER_CONFIG.unitString = "deg";
+    }
+}
---
+++ src/main/java/io/github/tigerbotics7125/robot/subsystem/Wrist.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2023 Tigerbotics and it's members. All rights reserved.
+ * This work is licensed under the terms of the GNU GPLv3 license
+ * found in the root directory of this project.
+ */
+package io.github.tigerbotics7125.robot.subsystem;
+
+import static io.github.tigerbotics7125.robot.constants.WristConstants.*;
+
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.hal.SimDouble;
+import edu.wpi.first.hal.simulation.SimDeviceDataJNI;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
+import io.github.tigerbotics7125.robot.constants.RobotConstants;
+
+public class Wrist extends ProfiledPIDSubsystem {
+
+    public enum State {
+        HOME(new Rotation2d()),
+        GROUND_INTAKE(new Rotation2d()),
+
+        MIN(new Rotation2d()),
+
+        // max
+        PARALLEL_ARM(new Rotation2d()),
+        ;
+
+        private Rotation2d mRotation;
+
+        private State(Rotation2d rotation) {
+            mRotation = rotation;
+        }
+    }
+
+    private final CANSparkMax mWrist = new CANSparkMax(MOTOR_ID, MOTOR_TYPE);
+    private final CANCoder mCANCoder = new CANCoder(CANCODER_ID);
+
+    private State mState = State.HOME;
+
+    public Wrist() {
+        super(PID);
+
+        configMotor(mWrist);
+
+        mCANCoder.configAllSettings(CANCODER_CONFIG);
+        mCANCoder.setPosition(
+                -(CANCODER_POS_CONV_FACTOR
+                        * (mCANCoder.getAbsolutePosition() - ABSOLUTE_HOME_DEG)));
+    }
+
+    public void configMotor(CANSparkMax motor) {
+        motor.restoreFactoryDefaults();
+
+        motor.setIdleMode(IdleMode.kCoast);
+        motor.setInverted(true);
+        motor.setSmartCurrentLimit(40);
+        motor.enableVoltageCompensation(RobotConstants.NOMINAL_VOLTAGE);
+
+        RelativeEncoder enc = motor.getEncoder();
+        enc.setPositionConversionFactor(POS_CONV_FACTOR);
+        enc.setVelocityConversionFactor(VEL_CONV_FACTOR);
+    }
+
+    @Override
+    protected void useOutput(double output, TrapezoidProfile.State setpoint) {
+        mWrist.setVoltage(output * 12D);
+    }
+
+    @Override
+    public double getMeasurement() {
+        return Units.degreesToRadians(mCANCoder.getPosition()) * CANCODER_POS_CONV_FACTOR;
+    }
+
+    public State getState() {
+        return mState;
+    }
+
+    public CommandBase setState(State state) {
+        return runOnce(
+                () -> {
+                    this.enable();
+                    mState = state;
+                    setGoal(state.mRotation.getRadians());
+                });
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        int dev = SimDeviceDataJNI.getSimDeviceHandle("SPARK MAX [" + mWrist.getDeviceId() + "]");
+        SimDouble preVel =
+                new SimDouble(SimDeviceDataJNI.getSimValueHandle(dev, "Velocity Pre-Conversion"));
+        SimDouble vel = new SimDouble(SimDeviceDataJNI.getSimValueHandle(dev, "Velocity"));
+
+        double neoVel = RobotConstants.NEO_FREE_SPEED_RPM;
+        double motorVel = MathUtil.clamp(mWrist.getAppliedOutput(), -1, 1) * neoVel;
+
+        preVel.set(motorVel);
+        vel.set(preVel.get() * VEL_CONV_FACTOR);
+        double masterPos = vel.get() * .02;
+        mWrist.getEncoder().setPosition(mWrist.getEncoder().getPosition() + masterPos);
+    }
+}
